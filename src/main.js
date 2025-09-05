632743c0eea24634c55741ccc0e0814727db5962
SEMIN-97 <jsemin.1021@gmail.com>
2025-09-05 22:36:56 +0900

    feat: 자동차 경주를 5회 진행하며, 회차 마다 자동차의 궤적을 출력한다

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import { readLineAsync } from './utils/readlineAsync.js';
-import { Car } from './car.js';
+import { readLineAsync } from './utils/readlineAsync.js'
+import { Car } from './car.js'
 
 class Game {
+  static RACING_COUNT = 5
+
   constructor() {}
 
   async start() {
-    const name = await readLineAsync('경주할 자동차 이름을 입력하세요. \n');
-    const car = new Car(name);
+    const name = await readLineAsync('경주할 자동차 이름을 입력하세요. \n')
+    const car = new Car(name)
+
+    for (let i = 1; i <= Game.RACING_COUNT; i++) {
+      car.moveForward()
+      console.log(`${car.name} : ${'-'.repeat(car.position)}`)
+    }
   }
 }
 
-const game = new Game();
-await game.start();
+const game = new Game()
+await game.start()
