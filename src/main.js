12fd34cc84b8a3aea6f74266b9b1517c4ef92fbf
Semin <jsemin.1021@gmail.com>
2025-09-14 14:23:56 +0900

    feat: 회차를 거듭할 때마다 자동차 이름과 지나간 궤적을 출력한다

--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
 import { readLineAsync } from './utils/readlineAsync.js'
 import { Car } from './car.js'
+import { Race } from './race.js'
 
 class Game {
   #cars
-
-  constructor() {}
+  #race
 
   async start() {
     this.#cars = await this.getCars()
+    this.#race = new Race(this.#cars)
+    
+    this.play()
   }
 
   async getCars() {
     const inputNames = await readLineAsync('경주할 자동차 이름을 입력하세요. (구분 기호: 쉼표(,))\n')
     return inputNames.split(',').map(name => new Car(name))
+  }
+
+  play() {
+    for (let i = 1; i <= this.#race.round; i++) {
+      this.#race.playTurn()
+      this.printTurnResult()
+    }
+  }
+
+  printTurnResult() {
+    this.#cars.forEach(car => console.log(`${car.name} : ${'-'.repeat(car.position)}`))
+    console.log('')
   }
 }
 
